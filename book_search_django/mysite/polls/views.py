9c4f2fce03ea943c4ab585bd278788903ba99b08
b43646 <408514942@qq.com>
2016-10-25 06:03:38 -0700

    update book_search
    Signed-off-by: b43646 <408514942@qq.com>

--- book_search_django/mysite/polls/views.py
+++ book_search_django/mysite/polls/views.py
@@ -4,18 +4,47 @@ from django.core.urlresolvers import reverse
 from .models import Book
 # Create your views here.
 
-def index(request):
+book_list = []
+name_list = []
+author_list = []
+cls_list = []
+
+def get_name_list():
 	book_list = Book.objects.all()
-	context = {'book_list' : book_list}
+	for i in book_list:
+		if i.name not in name_list:
+			name_list.append(i.name)
+
+
+def get_author_list():
+	book_list = Book.objects.all()
+	for i in book_list:
+		if i.author not in author_list:
+			author_list.append(i.author)
+
+
+def get_cls_list():
+	book_list = Book.objects.all()
+	for i in book_list:
+		if i.cls not in cls_list:
+			cls_list.append(i.cls)
+
+
+def index(request):
+	get_name_list()
+	get_author_list()
+	get_cls_list()
+	print name_list, author_list,cls_list
+	context = {'name_list' : name_list, 'author_list':author_list,'cls_list':cls_list}
 	return render(request, 'polls/1.html', context)
 
 def search(request):
-	book_list = Book.objects.all()
+	book_list_search = Book.objects.all()
 	if request.POST['name']:
-		book_list = book_list.filter(name=request.POST['name'])
+		book_list_search = book_list_search.filter(name=request.POST['name'])
 	if request.POST['author']:
-		book_list = book_list.filter(author=request.POST['author'])
+		book_list_search = book_list_search.filter(author=request.POST['author'])
 	if request.POST['cls']:
-		book_list = book_list.filter(cls=request.POST['cls'])
-	context = {'book_list' : book_list}
+		book_list_search = book_list_search.filter(cls=request.POST['cls'])
+	context = {'name_list' : name_list, 'author_list':author_list,'cls_list':cls_list, 'book_list_search':book_list_search}
 	return render(request, 'polls/1.html', context)
